fb270d38f78afc6659fd567a07001fc844d8b66e
SomeRandomAleks <74792495+SomeRandomAleks@users.noreply.github.com>
2021-02-05 19:43:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import json
 import typing
 import keep_alive
 import time
+import datetime
+import threading
 
 intents = discord.Intents.all()
 intents.members = True
@@ -13,18 +15,21 @@ intents.reactions = True
 client = commands.Bot(command_prefix="!", intents=intents)
 client.remove_command("help")
 
+
+
 @client.event
 async def on_ready():
 	print("Bot is ready")
 	await client.change_presence(activity=discord.Game("!help for all commands"))
-	embed = discord.Embed(description="Help us bump the server! By reacting to the 🔔 icon you will get pinged whenever it is time to bump\nReact again to not get bump notifications", color=0x1f4454)
-	msg = await client.get_channel(797342261507391488).send(embed=embed)
-	await msg.add_reaction("🔔")
 	while True:
-		await asyncio.sleep(20)
-		with open('spam.txt', 'r+') as f:
-			f.truncate(0)
+	    await asyncio.sleep(20)
+	    with open('spam.txt', 'r+') as f:
+		    f.truncate(0)
 
+
+
+
+keep_alive.keep_alive()
 
 @client.listen('on_message')
 async def spam(message):
@@ -41,6 +46,10 @@ async def spam(message):
             embed = discord.Embed(title=f"{message.author} has been muted", description="Reason: Spam\nThey will be unmuted in 30 seconds", color=0x1f4454)
             embed.set_thumbnail(url=message.author.avatar_url)
             embed.set_footer(text=time.ctime())
+            user_msgs = await message.channel.history(limit=15).flatten()
+            for msg in user_msgs:
+                if msg.author.id == message.author.id:
+                    await msg.delete()
             await message.channel.send(embed=embed)
             await asyncio.sleep(30)
             await message.author.remove_roles(role)
@@ -48,14 +57,30 @@ async def spam(message):
             em.set_thumbnail(url=message.author.avatar_url)
             em.set_footer(text=time.ctime())
             await message.channel.send(embed=em)
-        
+
+
+
+
+client.load_extension('cogs.bump')
+
+@client.listen('on_message')
+async def user_bump(message):
+    if message.content == "!d bump":
+        await message.delete()
+    
+
+
+
             
 
+                
+            
+    
 
 
 
 
-keep_alive.keep_alive()
+
 
 @client.event
 async def on_member_join(member):
@@ -64,13 +89,13 @@ async def on_member_join(member):
 	channel = client.get_channel(804882935227744266)
 	await channel.edit(name=f"Member Count: {guild_id.member_count}")
 
-	em = discord.Embed(title=f"Welcome ALVA | {member}", description=f"Please verify yourself in {client.get_channel(803826776970231888).mention} to see the rest of the server", color=0x1f4454)
+	em = discord.Embed(title=f"Welcome {member}", description=f"Please verify yourself in {client.get_channel(803826776970231888).mention} to see the rest of the server", color=0x1f4454)
 	em.set_footer(text=f"We now have {guild_id.member_count} members!")
 	em.set_thumbnail(url=member.avatar_url)
 
 	await client.get_channel(796139098086703148).send(embed=em)
 
-bump_msg = ""
+
 
 @client.event
 async def on_member_remove(member):
@@ -78,63 +103,7 @@ async def on_member_remove(member):
 	channel = client.get_channel(804882935227744266)
 	await channel.edit(name=f"Member Count: {guild_id.member_count}")
 
-@client.listen('on_message')
-async def bump(message):
-	if "!d bump" in message.content:
-		if message.channel.id != 797342261507391488:
-			bump_msg = message.author
-			await message.delete()
-			return
 
-		else:
-			return
-
-	else:
-		return
-
-@client.listen('on_message')
-async def bumped(message):
-	if message.channel.id == 797342261507391488:
-		if message.author.id != 796195995284406292:
-			if message.author.id == 302050872383242240:
-				embed_content = message.embeds[0].description
-				bumping = discord.utils.get(message.guild.roles, id=803859911397474354)
-				if "Please wait" in embed_content or "please wait" in embed_content or "wait another" in embed_content:
-					print(embed_content)
-					await message.delete()
-					
-					pass
-				else:
-					await asyncio.sleep(1)
-					await message.delete()
-					embed = discord.Embed(title=f"Thanks {bump_msg}!", description=f"The server has been bumped! Next bump is in 2 hours\n", color=0x1f4454)
-					embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/796182201976356875/806529281232601107/pandaemoji_1.png')
-					await message.channel.send(embed=embed)
-					await asyncio.sleep(7200)
-					await client.get_channel(797342261507391488).send(f"It's time to bump! {bumping.mention}")
-			else:
-				await message.delete()
-				pass
-
-
-		else:
-			return
-	else:
-		return
-
-
-@client.listen('on_message')
-async def disboard_different_channel(message):
-	if message.author.id == 302050872383242240:
-		if message.channel.id != 797342261507391488:
-			await message.delete()
-			return
-
-		else:
-			return
-
-	else:
-		return
 
 @client.listen('on_message')
 async def not_verify(message):
@@ -164,33 +133,16 @@ async def verify(ctx):
 
 @client.event
 async def on_raw_reaction_add(payload):
-    if payload.message_id == 806750309689982977:
+    if payload.message_id == 807388629595848704:
         role = discord.utils.get(payload.member.guild.roles, name="Bump")
         await payload.member.add_roles(role)
-    elif payload.message_id == 806561041726439484:
-        role = discord.utils.get(payload.member.guild.roles, name="Developer")
-        people = discord.utils.get(payload.member.guild.roles, name="People")
-        member = payload.member
-
-        overwrites = {
-            role: discord.PermissionOverwrite(read_messages=True, send_messages=True),
-            member: discord.PermissionOverwrite(read_messages=True, send_messages=True),
-            people: discord.PermissionOverwrite(read_messages=False)
-        }
-        name = f"ticket {payload.member}"
-        category = discord.utils.get(payload.member.guild.categories, name="tickets")
 
 
-        await client.get_guild(796139098086703145).create_text_channel(name, overwrites=overwrites, category=category)
         
-        
-
-    else:
-        return
 
 @client.event
 async def on_raw_reaction_remove(payload):
-    if payload.message_id == 806750309689982977:
+    if payload.message_id == 807388629595848704:
         guild_id = payload.guild_id
         guild = client.get_guild(guild_id)
         role = discord.utils.get(guild.roles, name="Bump")
