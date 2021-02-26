680accf6413fe9f9e8d202f6f2945a3d639f82bb
SomeRandomAleks <74792495+SomeRandomAleks@users.noreply.github.com>
2021-02-26 17:39:06 -0500

    Defs & variables

--- cogs/resources.py
+++ cogs/resources.py
@@ -1,12 +1,16 @@
 import discord
 from discord.ext import commands
+import datetime
 
 intents = discord.Intents.all()
 intents.members = True
 intents.reactions = True
 
 client = commands.Bot(command_prefix="!", intents=intents)
-
+python_hub = "https://cdn.discordapp.com/attachments/814986274158805033/814986385429758012/Python_Hub_1.png"
+guild = client.get_guild(796139098086703145)
+datetime_now = datetime.datetime.now()
+current_time = datetime_now.strftime("%H:%M")
 
 
 async def get_category(guild, category) -> discord.CategoryChannel:
